93e09ba2552f9bb9a0fa74bc3416946eb5d2c04a
Mitch Alexander <mitchella111@hotmail.com>
2023-03-15 18:17:25 +1000

    update to secret key

--- labmanager/settings.py
+++ labmanager/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,10 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!@%d_v)1i5r^+ny7zs+9k0bz@&@y241r)u$960sl$76$qgah#e'
+#SECRET_KEY = 'django-insecure-!@%d_v)1i5r^+ny7zs+9k0bz@&@y241r)u$960sl$76$qgah#e'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 #ALLOWED_HOSTS = ['http://0.0.0.0:8000/']
 ALLOWED_HOSTS = []
