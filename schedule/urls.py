6e5e1df6c0d963ac8dd2ce26634b94b3f7f68e7b
Mitch Alexander <mitchella111@hotmail.com>
2023-03-19 20:42:21 +1000

    add schedule pm

--- schedule/urls.py
+++ schedule/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = "schedule_app"
 urlpatterns = [
     path('schedule_home', views.schedule_home, name='schedule_home'),
+    path('schedule_pm', views.schedule_pm, name='schedule_pm'),
 
 
     ]
--- schedule/views.py
+++ schedule/views.py
@@ -77,3 +77,56 @@ def schedule_home(request):
     }
     return render(request, 'schedule/schedule_home.html', context)
 
+@login_required(login_url='home_app:login')
+def schedule_pm(request):
+
+        data_gantt = {'Tasks': [], 'Start': [], 'Finish': []}
+        task_list = Task.objects.all().exclude(Q(task_status='Completed') | Q(task_status='Cancelled')).order_by(
+            'project', 'task_critical_path', 'task_suffix')
+        for task in task_list:
+            concat_project_task = f'{task.project} {task.task_name} ({task.task_suffix}) {task.task_shift}'
+            data_gantt['Tasks'].append(concat_project_task)
+            #data_gantt['Resource'].append(task.task_assigned_to)
+            data_gantt['Start'].append(task.task_start_date)
+            data_gantt['Finish'].append(task.task_end_date)
+
+        df = pd.DataFrame(data_gantt)
+        fig = px.timeline(df,
+                          x_start="Start",
+                          x_end="Finish",
+                          y="Tasks",
+                          #color="Resource",
+
+                          labels={
+                              'x': 'Date',
+                              'y': 'Tasks',
+                          })
+
+        # set the x axis to show all days/dates instead of skipping -
+        # https://stackoverflow.com/questions/34755707/how-to-show-all-x-axis-tick-values-in-plotly
+        fig.update_xaxes(tickmode='linear', tickformat='%d %b (%a)', tickangle=300)
+        fig.update_yaxes(autorange="reversed")
+        # Loop to show a multilayerd gantt with a morning or afternoon wrap to help visualise if a job is less than a day.
+        # Still need to add code to change the morning/afternoon colours
+        # to a fixed colour for each to help with consistencies.
+
+        # for i in range(len(df.index)):
+        #     if df.iloc[i].Resource == 'M':
+        #         fig.data[i].width=1.1
+        #     if df.iloc[i].Resource == 'A':
+        #         fig.data[i].width = 1.1
+        #
+        # fig.update_layout(title={
+        #     'font_size': 22,
+        #     'xanchor': 'center',
+        #     'x': 0.5
+        # })
+        # fig.update_yaxes(autorange="reversed")
+        chart = fig.to_html()
+
+        context = {
+            "chart": chart,
+
+        }
+        return render(request, 'schedule/schedule_home.html', context)
+
