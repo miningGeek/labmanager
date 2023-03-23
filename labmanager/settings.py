ea938ef18b04d2b6dc747523ca237e0dc06c5d12
Mitch Alexander <mitchella111@hotmail.com>
2023-03-23 22:56:33 +1000

    change settings

--- labmanager/settings.py
+++ labmanager/settings.py
@@ -25,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 #SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!@%d_v)1i5r^+ny7zs+9k0bz@&@y241r)u$960sl$76$qgah#e'
-#SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+#SECRET_KEY = 'django-insecure-!@%d_v)1i5r^+ny7zs+9k0bz@&@y241r)u$960sl$76$qgah#e'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+#DEBUG = True
 
 #ALLOWED_HOSTS = ['*']
 ALLOWED_HOSTS = ['core-labmanager.up.railway.app','127.0.0.1']
